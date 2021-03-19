250d85da1d847eb9f274adae250101fd31079699
Rosellinda <64804606+Rosellinda@users.noreply.github.com>
2021-03-19 23:57:50 +0800

    Add API

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+
 function Home() {
+  React.useEffect(() => {
+    getProducts()
+  }, [])
+
+  function getProducts(){
+    const url = ''
+    axios.get(url)
+  }
+
   return <>home</>;
 }
 
